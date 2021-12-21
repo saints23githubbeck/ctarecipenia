8c2dfa2b68978a5472a1fb0652d7799278813b84
Shorif4321 <shorifali4321@gmail.com>
2021-12-21 22:27:18 +0600

    last

--- src/components/CommunityContainer/Community/Community.js
+++ src/components/CommunityContainer/Community/Community.js
@@ -4,10 +4,12 @@ import InstagramIcon from '@mui/icons-material/Instagram';
 import TwitterIcon from '@mui/icons-material/Twitter';
 import FacebookIcon from '@mui/icons-material/Facebook';
 import PinterestIcon from '@mui/icons-material/Pinterest';
+import { NavLink } from 'react-router-dom';
 const Community = ({ community }) => {
     const { name, photo, description, facebook, twitter, instagram, pinterest } = community;
     return (
         <div className='col-md-4 p-4 mt-4'>
+            <NavLink style={{textDecoration:'none', color:'black'}} to="/profile">
             <div className=' text-center community'>
                 <img className="img-fluid" src={photo} alt="" />
                 <h5>{name}</h5>
@@ -20,6 +22,7 @@ const Community = ({ community }) => {
                     <a href={twitter} target="_blank" rel="noreferrer"><TwitterIcon style={{ margin: "0 4px", color: "lightblue", fontSize: "27px" }} /></a>
                 </div>
             </div>
+            </NavLink>
         </div>
     );
 };
