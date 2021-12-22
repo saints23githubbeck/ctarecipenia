80af97a84afa5b41e43f00dd8bf8277c9166e023
Shorif4321 <shorifali4321@gmail.com>
2021-12-22 11:56:12 +0600

    EmailSubscribeForm

--- src/Components/Dashboard/EmailSubscribers/SubscribersEmail.jsx
+++ src/Components/Dashboard/EmailSubscribers/SubscribersEmail.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
+import './SubscribersEmail.css'
 
-const SubscribersEmail = () => {
-    return (
-        <div>
-            <h1>Send Email to Subscribers</h1>
-            
+const SubscribersEmail = () => (
+    <div>
+        <h1 className='fs-1 Subscribers-heading'>Send Email to Subscribers</h1>
+
+        <div className='Subscribers-from my-5'>
+            <div className=''>
+                <h5 className='mb-3'>Subject</h5>
+                <input className='mb-2' type="text" />
+                <hr className='my-3' />
+                <h5 className='mb-3'>Content</h5>
+                <textarea name="" id="" cols="30" rows="15"></textarea> <br />
+                <div className='buttonCustom'>
+                    <button className='Subscribers-button mt-3'>Send Email</button>
+                </div>
+            </div>
         </div>
-    );
-};
+
+    </div>
+);
 
 export default SubscribersEmail;
