8f5939999f05a46aa3f4178f49012faff33c8722
bitue <ashikul.islam_ashik@yahoo.com>
2021-12-27 18:11:41 +0600

    all-done

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import DashboardRoot from '../DashboardRoot/DashboardRoot';
+import DashboardSlide from '../DashboardSlide/DashboardSlide';
 import ManageAds from '../ManageAds/ManageAds';
 import MyRecipes from '../MyRecipes/MyRecipes';
 import NewsLetters from '../Newsletters/NewsLetters';
@@ -10,6 +11,8 @@ import Profile from '../Profile/Profile';
 const Dashboard = () => {
     return (
         <div>
+        
+            <DashboardSlide></DashboardSlide>
             <NewsLetters></NewsLetters>
             <ManageAds></ManageAds>
             <DashboardRoot></DashboardRoot>
