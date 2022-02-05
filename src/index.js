903f7f7553184fc31f47c549703e694f2d0e32cd
mohammed kabir <kbshowkb@gmail.com>
2022-02-05 20:35:16 +0100

    added redux management

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import "./index.css"
-import App from "./App"
-import reportWebVitals from "./reportWebVitals"
-import "bootstrap/dist/css/bootstrap.min.css"
+import React from "react";
+import ReactDOM from "react-dom";
+import reportWebVitals from "./reportWebVitals";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Provider } from "react-redux";
+
+import { store } from "./appState/store";
+import "./index.css";
+import App from "./App";
+
+
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById("root")
 )
 
