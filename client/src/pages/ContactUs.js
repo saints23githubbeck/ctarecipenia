2dc6b4fdd3d6d655f493bf4bb5328df4aee997c5
mohkabir <kbshowkb@gmail.com>
2022-04-14 09:28:08 +0100

    fix merge

--- client/src/pages/ContactUs.js
+++ client/src/pages/ContactUs.js
@@ -102,58 +102,6 @@ const ContactUs = () => {
 
       <StayWithUs/>
     </section>
-  //  <section className='contact'>
-  //     <div className="contact-nav">
-  //         <div className="nav-text">          
-  //           <h1>Contact Us</h1>
-  //           <ul className="list">
-  //             <li>Home</li>
-  //             <li>contact Us</li>
-  //           </ul>
-  //         </div>
-  //     </div>
-  //     <div className="wrapper">
-  //         <div className="img">
-  //           <img src={contactImg} alt="" />
-  //         </div>
-  //     </div>
-  //     <div className="question">
-  //       <div className="question-text">
-  //       <p>Get in touch with any questions</p>
-  //       </div>
-  //       <div className="question-contact">
-  //           <div className="phone">
-  //               <div className="img">
-  //               <img src={phone} alt=""/>
-  //               </div>
-  //               <p>Phone</p>
-  //               <h6>038921783</h6>
-  //           </div>
-  //           <div className="mail">
-  //               <div className="im">
-  //               <img src={mail} alt=""/>
-  //               </div>
-  //               <p>
-  //               Email
-  //               </p>
-  //               <h6>
-  //               a@recipimania.com
-  //               </h6>
-  //           </div>
-  //           <div className="music">
-  //               <div className="img">
-  //               <img src={music} alt=""/>
-  //               </div>
-  //               <p>
-  //               Support
-  //               </p>
-  //               <h6>
-  //               04u3i372
-  //               </h6>
-  //           </div>
-  //       </div>
-  //     </div>
-  //  </section>
   )
 }
 
