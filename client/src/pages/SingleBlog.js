5137467eed9d0786047b26e53b4014fad578f098
Abdulsalam24 <abdulsalammohammed586686@gmail.com>
2022-04-14 08:02:25 -0700

    finished user dashboard

--- client/src/pages/SingleBlog.js
+++ client/src/pages/SingleBlog.js
@@ -137,125 +137,125 @@ const SingleBlog = () => {
         </div>
       </div>
       <div className="wrapper">
-      <div className="max-width">
-        <div className="single">
-          <div className="single-item">
-            <div className="text">
-              <h3>{title}</h3>
-              <div className="info flex">
-                <div className="view flex">
-                  <img src={eyeColor} alt="" /> <span>{view}</span>
+        <div className="max-width">
+          <div className="single">
+            <div className="single-item">
+              <div className="text">
+                <h3>{title}</h3>
+                <div className="info flex">
+                  <div className="view flex">
+                    <img src={eyeColor} alt="" /> <span>{view}</span>
+                  </div>
+                  <div className="clock flex">
+                    <img src={clockColor} alt="" />
+                    <span>Last Update : {lastUpdate}</span>
+                  </div>
+                  <div className="calender flex">
+                    <img src={calenderColor} alt="" />
+                    <span>Created : {created}</span>
+                  </div>
                 </div>
-                <div className="clock flex">
-                  <img src={clockColor} alt="" />
-                  <span>Last Update : {lastUpdate}</span>
-                </div>
-                <div className="calender flex">
-                  <img src={calenderColor} alt="" />
-                  <span>Created : {created}</span>
-                </div>
               </div>
+              <div className="img">
+                <img src={img} alt="" />
+              </div>
+              <p>{text}</p>
             </div>
-            <div className="img">
-              <img src={img} alt="" />
-            </div>
-            <p>{text}</p>
           </div>
         </div>
-      </div>
-      <h3 className="left-h3">You may also like</h3>
-      <div className="card_wrapper">
-        {latestData.map((recipe, index) => (
-          <div key={index} className="card">
-            <div>
-              <img src={recipe.recipeImage} alt="recipeImage" />
-            </div>
-            <h3>{recipe.title}</h3>
-            <div>
-              <img src={recipe.ownerImage} alt={recipe.ownerName} />
-              <span>
-                <strong>{recipe.ownerName}</strong>
-              </span>
-            </div>
-            <div className="rating" style={{ textAlign: "center" }}>
-              <span className="rated">&#9733;</span>
-              <span className="rated">&#9733;</span>
-              <span className="rated">&#9733;</span>
-              <span>&#9733;</span>
-            </div>
-          </div>
-        ))}
-      </div>
-      <div className="max-width">
-        <div className="center">
-          <h1>What do you think?</h1>
-        </div>
-        <div className="left">
-          <h1>23 Responses</h1>
-        </div>
-        <div className="icons flex">
-          <img src={frown} alt="" />
-          <img src={thumb} alt="" />
-          <img src={smile} alt="" />
-        </div>
-
-        <div className="comment flex">
-          <p>0 Comments</p>
-          <p>Bambam</p>
-          <p>
-            <img src={lock} alt="" /> Disqus’Privacy Policy
-          </p>
-        </div>
-        <div className="favorite flex">
-          <img src={love} alt="" />
-          <p>Favorite</p>
-          <span className="number">1</span>
-        </div>
-        <form>
-          <div className="log-in flex">
-            <div className="user">
-              <img src={user} alt="" />
-            </div>
-            <div className="input">
-              <input type="text" placeholder="Start the discussion..." />
-              <p>LOG IN WITH</p>
-            </div>
-          </div>
-          <div className="sign-up flex">
-            <div className="social flex">
-              <div className="fb">
-                <img src={facebook} alt="" />
+        <h3 className="left-h3">You may also like</h3>
+        <div className="card_wrapper">
+          {latestData.map((recipe, index) => (
+            <div key={index} className="card">
+              <div>
+                <img src={recipe.recipeImage} alt="recipeImage" />
               </div>
-              <div className="tw">
-                <img src={twitter} alt="" />
+              <h3>{recipe.title}</h3>
+              <div>
+                <img src={recipe.ownerImage} alt={recipe.ownerName} />
+                <span>
+                  <strong>{recipe.ownerName}</strong>
+                </span>
               </div>
-              <div className="ig">
-                <img src={instagram} alt="" />
+              <div className="rating" style={{ textAlign: "center" }}>
+                <span className="rated">&#9733;</span>
+                <span className="rated">&#9733;</span>
+                <span className="rated">&#9733;</span>
+                <span>&#9733;</span>
               </div>
             </div>
-            <div className="input">
-              <p>OR SIGN UP WITH DISQUS ?</p>
-              <input type="text" placeholder="Name" />
-            </div>
-          </div>
+          ))}
+        </div>
+        <div className="max-width">
           <div className="center">
-            <h1>Be the first to comment.</h1>
+            <h1>What do you think?</h1>
           </div>
-        </form>
-        <div className="prefooter flex">
-          <div className="mail">
-            <img src={mail} alt="" /> <span>Subscribe</span>
+          <div className="left">
+            <h1>23 Responses</h1>
           </div>
-          <div className="d">
-            <span className="d-img">D</span>
-            <span>Add Disqus to your site</span>
+          <div className="icons flex">
+            <img src={frown} alt="" />
+            <img src={thumb} alt="" />
+            <img src={smile} alt="" />
           </div>
-          <div className="danger">
-            <img src={danger} alt="" /> <span>Do Not Sell My Data</span>
+
+          <div className="comment flex">
+            <p>0 Comments</p>
+            <p>Bambam</p>
+            <p>
+              <img src={lock} alt="" /> Disqus’Privacy Policy
+            </p>
           </div>
+          <div className="favorite flex">
+            <img src={love} alt="" />
+            <p>Favorite</p>
+            <span className="number">1</span>
+          </div>
+          <form>
+            <div className="log-in flex">
+              <div className="user">
+                <img src={user} alt="" />
+              </div>
+              <div className="input">
+                <input type="text" placeholder="Start the discussion..." />
+                <p>LOG IN WITH</p>
+              </div>
+            </div>
+            <div className="sign-up flex">
+              <div className="social flex">
+                <div className="fb">
+                  <img src={facebook} alt="" />
+                </div>
+                <div className="tw">
+                  <img src={twitter} alt="" />
+                </div>
+                <div className="ig">
+                  <img src={instagram} alt="" />
+                </div>
+              </div>
+              <div className="input">
+                <p>OR SIGN UP WITH DISQUS ?</p>
+                <input type="text" placeholder="Name" />
+              </div>
+            </div>
+            <div className="center">
+              <h1>Be the first to comment.</h1>
+            </div>
+          </form>
+          <div className="prefooter flex">
+            <div className="mail">
+              <img src={mail} alt="" /> <span>Subscribe</span>
+            </div>
+            <div className="d">
+              <span className="d-img">D</span>
+              <span>Add Disqus to your site</span>
+            </div>
+            <div className="danger">
+              <img src={danger} alt="" /> <span>Do Not Sell My Data</span>
+            </div>
+          </div>
+          <h1 className="right">DISQUS</h1>
         </div>
-        <h1 className="right">DISQUS</h1>
-      </div>
       </div>
     </section>
   );
