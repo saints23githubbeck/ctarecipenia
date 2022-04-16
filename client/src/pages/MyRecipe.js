bbf8f8685c59e2f563ac977040044421f383d6a2
Abdulsalam24 <abdulsalammohammed586686@gmail.com>
2022-04-16 08:14:10 -0700

    designing myrecipe page

--- client/src/pages/MyRecipe.js
+++ client/src/pages/MyRecipe.js
@@ -1,4 +1,4 @@
-import React , {useState} from 'react'
+import React, { useState } from "react";
 
 import "../assets/styles/myRecipe.scss";
 
@@ -8,145 +8,117 @@ import UserProfile from "../components/UserProfile";
 
 import view from "../assets/images/view.png";
 import edit from "../assets/images/edit.png";
+import { Link } from 'react-router-dom';
 
 const MyRecipe = () => {
-    const [recipesList , setRecipesList] = useState([
-        {
-            id : 1,
-            photo : ownerImage,
-            title : 'Garlic Chicken',
-            category : 'Food',
-            view: false
-        },
-        {
-            id : 2,
-            photo : ownerImage,
-            title : 'Garlic Chicken',
-            category : 'Food',
-            view: false
-        },
-        {
-            id : 3,
-            photo : ownerImage,
-            title : 'Garlic Chicken',
-            category : 'Food'
-        },
-        {
-            id : 4,
-            photo : ownerImage,
-            title : 'Garlic Chicken',
-            category : 'Food',
-            view: false
-        },
-        {
-            id : 5,
-            photo : ownerImage,
-            title : 'Garlic Chicken',
-            category : 'Food',
-            view: false
-        },
-        {
-            id : 6,
-            photo : ownerImage,
-            title : 'Garlic Chicken',
-            category : 'Food',
-            view: false
-        }
-    ])
-    const [ changeText , setchangeText] = useState(true)
+  const [recipesList, setRecipesList] = useState([
+    {
+      id: 1,
+      photo: ownerImage,
+      title: "Garlic Chicken",
+      category: "Food",
+    },
+    {
+      id: 2,
+      photo: ownerImage,
+      title: "Garlic Chicken",
+      category: "Food",
+    },
+    {
+      id: 3,
+      photo: ownerImage,
+      title: "Garlic Chicken",
+      category: "Food",
+    },
+    {
+      id: 4,
+      photo: ownerImage,
+      title: "Garlic Chicken",
+      category: "Food",
+    },
+    {
+      id: 5,
+      photo: ownerImage,
+      title: "Garlic Chicken",
+      category: "Food",
+    },
+    {
+      id: 6,
+      photo: ownerImage,
+      title: "Garlic Chicken",
+      category: "Food",
+    },
+  ]);
 
-    const handleDelete = (e) => {
-        console.log(e)
-        const filtered = recipesList.filter(recipe => recipe !== e);
-        setRecipesList(filtered)
-    }
-
-    const handleEdit = (e) => {
-        e.map((h) => {
-
-        })
-    }
-
-    console.log(changeText , 'change')
-
+  const handleDelete = (e) => {
+    console.log(e);
+    const filtered = recipesList.filter((recipe) => recipe !== e);
+    setRecipesList(filtered);
+  };
 
   return (
-    <section className='myrecipe'>
-        <div className="sub-nav">
-            <div className="flex wrapper">
-                <h1>My Recipes</h1>
-                <ul className="list flex">
-                    <li>Home</li>
-                    <li>My Recipe</li>
-                </ul>
-            </div>
+    <section className="myrecipe">
+      <div className="sub-nav">
+        <div className="flex wrapper">
+          <h1>My Recipes</h1>
+          <ul className="list flex">
+            <li>Home</li>
+            <li>My Recipe</li>
+          </ul>
+        </div>
       </div>
       <div className="wrapper">
-      <UserProfile isProfile={false}/>
-          <div className="right">
-              <div className="recipe-list">
-                  <div className="heading">
-                  <h3>My Recipes</h3>
+        <UserProfile isProfile={false} />
+        <div className="right">
+          <div className="recipe-list">
+            <div className="heading">
+              <h3>My Recipes</h3>
+            </div>
+            <div className="caption">
+              <ul className="head grid">
+                <li className="row1">Photo</li>
+                <li className="row2">Title</li>
+                <li className="row3">Category</li>
+                <li className="row4">Status</li>
+                <li className="row5">Action</li>
+              </ul>
+            </div>
+            <div className="recipes flex">
+              {recipesList.map((recipe) => (
+                <div key={recipe.id} className="recipe grid">
+                  <div className="row1">
+                    <img src={recipe.photo} alt="" />
                   </div>
-                  <div className="caption">
-                      <ul className="head grid">
-                          <li className='row1'>Photo</li>
-                          <li className='row2'>Title</li>
-                          <li className='row3'>Category</li>
-                          <li className='row4'>Status</li>
-                          <li className='row5'>Action</li>
-                      </ul>
-                  </div>
-                  <div className="recipes flex">
-                    {
-                        recipesList.map(recipe => (
-                            <div key={recipe.id} className='recipe grid'>
-                                <div className="row1">
-                                    <img src={recipe.photo} alt="" />
-                                </div>
 
-                                {
-                                    changeText ? (
-                                        <>
-                                            <div className="row2">
-                                            <h5>{recipe.title}</h5>
-                                            </div>
-                                            <div className="row3">
-                                                <h5>{recipe.category}</h5>
-                                            </div>
-                                        </>
-                                    ) : (
-                                        <>
-                                            <div className="row2">
-                                            <input type="text" />
-                                        </div>
-                                        <div className="row3">
-                                            <input type="text" />
-                                        </div>
-                                        </>
-                                    )
-                                }
-
-                                <div className="row5 flex actions">
-                                    <div className="view"><img src={view} alt="" /></div>
-                                    <div onClick={(e) => handleEdit(recipe)}
-                                     className="edit">
-                                         <img src={edit} alt="" />
-                                    </div>
-                                    
-                                    <div onClick={(e) => handleDelete(recipe)} className="delete">
-                                        <img src={view} alt="" />
-                                    </div>
-                                </div>
-                            </div>
-                        ))
-                    }
+                  <div className="row2">
+                    <h5>{recipe.title}</h5>
                   </div>
-              </div>
+                  <div className="row3">
+                    <h5>{recipe.category}</h5>
+                  </div>
+                  <div className="row5 flex actions">
+                    <div className="view">
+                      <img src={view} alt="" />
+                    </div>
+                    <Link to='/addrecipe'>
+                    <div className="edit">
+                      <img src={edit} alt="" />
+                    </div></Link>
+                    <div
+                      onClick={(e) => handleDelete(recipe)}
+                      className="delete"
+                    >
+                      <img src={view} alt="" />
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
           </div>
+        </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default MyRecipe
+export default MyRecipe;
