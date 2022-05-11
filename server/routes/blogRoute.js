a4733b483d0335bfa4139b44f4be2377df6817e7
armoako <frank.amoako-dev@outlook.com>
2022-05-11 23:57:32 +0000

    added new routes

--- server/routes/blogRoute.js
+++ server/routes/blogRoute.js
@@ -5,14 +5,20 @@ const {
   createBlog,
   getBlogBySlug,
   deleteBlogBySlug,
+  searchBlog,
+  fetchRelatedBlogs,
+  fetchBlogByUser,
 } = require("../controllers/blogController")
 const { requireSignIn } = require("../middleware/authMiddleware")
 const router = express.Router()
 
-router.post("/blog/create", requireSignIn, createBlog)
+router.post("/user/blog/create", requireSignIn, createBlog)
+router.post("/blogs/related", fetchRelatedBlogs)
 router.get("/blogs", getBlogs)
 router.get("/blogs/:slug", getBlogBySlug)
-router.delete("/blogs/:slug", requireSignIn, deleteBlogBySlug)
-router.put("/blogs/:slug", requireSignIn, updateBlog)
+router.get("/:username/blogs", fetchBlogByUser)
+router.delete("/user/blogs/:slug", requireSignIn, deleteBlogBySlug)
+router.put("/user/blogs/:slug", requireSignIn, updateBlog)
+router.get("/blogs/search", searchBlog)
 
 module.exports = router
