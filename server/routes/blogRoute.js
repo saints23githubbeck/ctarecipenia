905306a03b79e4b59dd6c5c73cccebef38092daa
armoako <frank.amoako-dev@outlook.com>
2022-05-18 00:08:42 +0000

    major fix

--- server/routes/blogRoute.js
+++ server/routes/blogRoute.js
@@ -11,7 +11,7 @@ const {
   canDeleteBlog,
   canUpdateBlog,
 } = require("../controllers/blogController")
-const { requireSignIn } = require("../middleware/authMiddleware")
+const { requireSignIn, adminMiddleware, authMiddleware } = require("../middleware/authMiddleware")
 const router = express.Router()
 
 /** Public routes   */
@@ -23,28 +23,11 @@ router.get("/blogs/search", searchBlog)
 /** admin only route */
 router.post("/admin/blog", requireSignIn, adminMiddleware, createBlog)
 router.put("/admin/blog/:slug", requireSignIn, adminMiddleware, updateBlog)
-router.delete(
-  "admin/blog/:slug",
-  requireSignIn,
-  adminMiddleware,
-  deleteBlogBySlug
-)
+router.delete( "admin/blog/:slug", requireSignIn,adminMiddleware,deleteBlogBySlug)
 
 /** user only routes */
 router.post("/user/blog", requireSignIn, authMiddleware, createBlog)
-router.put(
-  "/user/blog/:slug",
-  requireSignIn,
-  authMiddleware,
-  canUpdateBlog,
-  updateBlog
-)
-router.delete(
-  "/user/blog/:slug",
-  requireSignIn,
-  authMiddleware,
-  canDeleteBlog,
-  deleteBlogBySlug
-)
+router.put( "/user/blog/:slug",requireSignIn,authMiddleware,canUpdateBlog, updateBlog)
+router.delete("/user/blog/:slug",requireSignIn,authMiddleware,canDeleteBlog,deleteBlogBySlug)
 router.get("/:username/blogs", fetchBlogByUser)
 module.exports = router
