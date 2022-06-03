b1ece08d89c53286cd5ec8b8856b5678aff3aaff
armoako <frank.amoako-dev@outlook.com>
2022-06-03 23:10:57 +0000

    minor fix

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -2,35 +2,44 @@ const asyncHandler = require("express-async-handler")
 const jwt = require("jsonwebtoken")
 const User = require("../models/userModel")
 
+const { TokenExpiredError } = jwt
+
+const catchError = (err, res) => {
+  if (err instanceof TokenExpiredError) {
+    return res.status(401).send({ message: "Unauthorized! Access Token was expired!" })
+  }
+
+  return res.sendStatus(401).send({ message: "Unauthorized!" })
+}
+
 exports.requireSignIn = asyncHandler(async (req, res, next) => {
   let token
 
-  if (
-    req.headers.authorization &&
-    req.headers.authorization.startsWith("Bearer")
-  ) {
+  if (req.headers.authorization && req.headers.authorization.startsWith("Bearer")) {
     try {
       //get the token from the headers
       token = req.headers.authorization.split(" ")[1]
+
       //verify the token
-      const accessToken = jwt.verify(token, process.env.JWT_SECRET)
-
+      jwt.verify(token, process.env.JWT_SECRET, (err, verifiedToken) => {
+        if (err) {
+          return catchError(err, res)
+        }
       //get user information from verified access token
-      // console.log("accessToken", accessToken)
-      const accessTokenId = accessToken.userId
-      req.user = await User.findById(accessTokenId)
+      // console.log("verifiedToken", verifiedToken)
+      const userId = verifiedToken.userId
+      req.user = await User.findById(userId)
 
-      next()
+        next()
+      })
     } catch (error) {
-      console.log(
-        `Error getting user information. Access denied: ${error.message}`
-      )
-     return res.status(401).json({ error: "Access Denied" })
+      console.log(`Error getting user information. Access denied: ${error.message}`)
+      return res.status(401).json({ error: "Access Denied" })
     }
   }
 
   if (!token) {
-   return res.status(401).json({ error: "Access Denied. Please login to continue" })
+    return res.status(401).json({ error: "Access Denied. Please login to continue" })
   }
 })
 
